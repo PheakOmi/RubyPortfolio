351963e7cac46cd13ffbbbab245211a3d8d1ffc2
Sopheak Dy <sopheakdy23@gmail.com>
2019-10-02 12:34:10 +0700

    Implemented content helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
 
   def login_helper
-
     if current_user.is_a?(User)
       link_to 'Logout', destroy_user_session_path, method: :delete
 
@@ -10,7 +9,13 @@ module ApplicationHelper
       "<br>".html_safe+
       (link_to 'Login', new_user_session_path)
     end
+  end
 
+  def source_helper (layout_name)
+    if session[:source]
+      greeting = "Thank you for visiting me through #{session[:source]}. You are now on #{layout_name} layout!"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
 
 end
